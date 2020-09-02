d03a1c121efa1697132d771dc6cc0a0d8ae1b5a9
kingz10102 <kingz10102@gmail.com>
2020-09-02 14:26:37 -0400

    this is a commit

--- src/main/java/com/example/finalproject/models/User.java
+++ src/main/java/com/example/finalproject/models/User.java
@@ -1,4 +1,45 @@
 package com.example.finalproject.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+//
+    @Column(unique = true)
+    private String username;
+
+    @Column
+    private String password;
+
+    public User() {}
+//getters
+    public Long getId() {
+        return id;
+    }
+//setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
 }
